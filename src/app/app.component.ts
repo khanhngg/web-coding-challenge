2ed3e21ff096a29e6809f74c6b8d0419d3bd9fae
khanhngg <khanhnguye94@gmail.com>
2017-12-26 15:10:03 -0800

    initial commit to setup node app

--- src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',//`<h1>Hello {{name}}</h1><github></github>`,
-})
-export class AppComponent  { name = 'Angular'; }
